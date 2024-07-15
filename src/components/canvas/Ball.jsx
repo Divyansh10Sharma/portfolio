63e84ab2d5604a1fd20225f719b439e30732a7e2
Divyansh_Sharma <111333281+Divyansh10Sharma@users.noreply.github.com>
2024-07-15 15:45:43 +0530

    Update Ball.jsx

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,40 +1,33 @@
-import React, { Suspense, useRef, useEffect } from "react";
+import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { Decal, Float, OrbitControls, Preload, useTexture } from "@react-three/drei";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+
 import CanvasLoader from "../Loader";
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
-  const meshRef = useRef();
-
-  useEffect(() => {
-    if (meshRef.current) {
-      const positionAttr = meshRef.current.geometry.attributes.position;
-      const positions = positionAttr.array;
-      for (let i = 0; i < positions.length; i++) {
-        if (isNaN(positions[i])) {
-          console.error(`Position attribute contains NaN at index ${i}`);
-        }
-      }
-      console.log("Position attribute values:", positions);
-    }
-  }, []);
 
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75} ref={meshRef}>
-        <sphereGeometry args={[1, 32, 32]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
-          color="#fff8eb"
+          color='#fff8eb'
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
         />
         <Decal
-          position={[0, 0, 1]}
-          rotation={[0, 0, 0]}
+          position={[0.1, 0.1, 1]}
+          rotation={[2 * Math.PI, 0.1, 6.25]}
           scale={1}
           map={decal}
           flatShading
@@ -47,7 +40,7 @@ const Ball = (props) => {
 const BallCanvas = ({ icon }) => {
   return (
     <Canvas
-      frameloop="demand"
+      frameloop='demand'
       dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
     >
@@ -55,6 +48,7 @@ const BallCanvas = ({ icon }) => {
         <OrbitControls enableZoom={false} />
         <Ball imgUrl={icon} />
       </Suspense>
+
       <Preload all />
     </Canvas>
   );
