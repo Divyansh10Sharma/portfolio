911bcf13bed0f32bc8e13bd316b2e45269f4b1e3
Divyansh_Sharma <111333281+Divyansh10Sharma@users.noreply.github.com>
2026-03-01 08:44:38 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
+import { HeroCanvas } from "./heroCanvas";
 
 const Hero = () => {
   return (
@@ -26,6 +27,7 @@ const Hero = () => {
       </div>
 
 {/*       <ComputersCanvas /> */}
+      <HeroCanvas />
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
