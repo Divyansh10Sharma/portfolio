0fa43b03703f65d1ef6ef8daf87d4c3583dec88d
Divyansh_Sharma <111333281+Divyansh10Sharma@users.noreply.github.com>
2026-03-01 08:47:57 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
-import { HeroCanvas } from "./heroCanvas";
+import HeroCanvas from "./heroCanvas";
 
 const Hero = () => {
   return (
