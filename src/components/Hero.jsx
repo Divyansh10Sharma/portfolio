7fdc1c03cc745dabb12c2fd983a0de31034d6ae9
Divyansh_Sharma <111333281+Divyansh10Sharma@users.noreply.github.com>
2026-03-01 08:51:47 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
       </div>
 
 {/*       <ComputersCanvas /> */}
-      <HeroCanvas />
+      {/* <HeroCanvas /> */}
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
